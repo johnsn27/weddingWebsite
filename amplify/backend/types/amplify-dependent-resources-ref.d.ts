378785ff6a727ada09c9f5c8b8bf4003ce328b11
Nathan Johnson <nathan.johnson@bbc.co.uk>
2024-08-02 17:59:47 +0100

    amplify backend setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "weddingwebsited34e09b7PostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
