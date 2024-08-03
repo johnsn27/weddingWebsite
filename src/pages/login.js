12ca8bd24d2660a263e64cb741c8c0c8da2c8cd1
Nathan Johnson <nathan.johnson@bbc.co.uk>
2024-08-03 10:23:36 +0100

    basic login and sign up page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
 import { Authenticator, ThemeProvider, defaultTheme } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -25,16 +26,41 @@ const customTheme = {
 };
 
 const Login = () => {
+  const navBarHeight = '80px'; // The height of your navbar
+
   return (
     <ThemeProvider theme={customTheme}>
-      <Authenticator>
-        {({ signOut, user }) => (
-          <main>
-            <h1>Welcome, {user.username}!</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        )}
-      </Authenticator>
+      <div css={{ paddingTop: navBarHeight }}>
+        <Authenticator
+          signUp={{
+            formFields: {
+              username: {
+                labelHidden: false,
+                placeholder: 'Enter your username',
+              },
+              password: {
+                labelHidden: false,
+                placeholder: 'Enter your password',
+              },
+              confirm_password: {
+                labelHidden: false,
+                placeholder: 'Confirm your password',
+              },
+              email: {
+                labelHidden: false,
+                placeholder: 'Enter your email',
+              },
+            },
+          }}
+        >
+          {({ signOut, user }) => (
+            <main>
+              <h1>Welcome, {user.username}!</h1>
+              <button onClick={signOut}>Sign out</button>
+            </main>
+          )}
+        </Authenticator>
+      </div>
     </ThemeProvider>
   );
 };
