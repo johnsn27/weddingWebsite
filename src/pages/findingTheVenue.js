68c4a4665aa0cd4fef85b25b5adc675efb572a53
Nathan Johnson <nathan.johnson@bbc.co.uk>
2024-08-06 18:52:36 +0100

    update page

--- src/pages/findingTheVenue.js
+++ src/pages/findingTheVenue.js
@@ -1,27 +1,26 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
-import { css } from '@emotion/react';
+import { storyPageStyle, titleStyle, paragraphStyle } from '../styles/pageStyles';
 
-const FindingTheVenue = () => {
-  return (
-    <div css={storyPageStyle}>
-      <h1 css={titleStyle}>Local Accommodation</h1>
-      <p css={paragraphStyle}>
-        <a href="http://www.theoystersmackinn.co.uk" css={linkStyle}>The Oyster Smack Inn</a> | 01621 782141<br />
-        <a href="http://www.theshipinnburnham.co.uk" css={linkStyle}>The Ship Inn</a> | 01621 785057<br />
-        <a href="http://www.peaberriescoffee.co.uk" css={linkStyle}>Mrs Watsons Accommodation</a> | 01621 929477<br />
-        <a href="http://www.burntmillcottagebandb.com" css={linkStyle}>Burnt Mill Cottage</a> | 01621 783712<br />
-        <a href="http://www.homeaway.co.uk/p6492691" css={linkStyle}>Town House</a> | 07867538875<br />
-        <a href="http://www.ferryboatinnessex.com" css={linkStyle}>The Ferry Boat Inn</a> | 01621 738844<br />
-        <a href="http://www.travelodge.co.uk/hotels/669/Maldon-hotel" css={linkStyle}>Travelodge Maldon</a>
-      </p>
-      <br />
-      <p css={paragraphStyle}>
-        A very thorough accommodation list for the area can be found on the Maldon District Council website: <a href="http://www.visitmaldondistrict.co.uk/places-to-stay" css={linkStyle}>www.visitmaldondistrict.co.uk/places-to-stay</a>.<br />
-        The area is popular for weekend breaks, we recommend reserving accommodation as soon as possible.
-      </p>
-    </div>
-  );
+const Taxis = () => {
+    return (
+        <div css={storyPageStyle}>
+          <h1 css={titleStyle}>Finding the venue</h1>
+          <p css={paragraphStyle}>
+          <b>Address: High House Weddings, Old Heath Road, Althorne, Essex CM3 6EW</b> <br />
+          High House is located between the Althorne and Burnham on Crouch.
+          When arriving from the direction of Maldon / Chelmsford you will approach High House via the B1010. 
+          On passing through the village of Althorne turn left onto The Endway. 
+          High House is on the right, approximately one mile from this point and well signposted on arrival.
+          </p>
+          <br />
+          <p css={paragraphStyle}>
+          <b>FOR DRIVERS AND TAXIS</b> - when booking taxis and driving to High House please ensure your location is noted as High House Wedding Venue. <br />
+          This ensures our local neighbours are not disturbed when guests and taxis are locating the venue. 
+          Unfortunately Google Maps and Sat Nav systems can be unclear as to our exact location and will generally take you too far.
+          Please look out for our entrance rather than relying on these.
+          </p>
+        </div>
+      );
 };
 
-export default FindingTheVenue;
+export default Taxis;
